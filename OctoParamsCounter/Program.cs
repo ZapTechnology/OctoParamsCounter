c5432236363fc0477e55b70181721db97fde2b08
zaptech\jun.zhu <jun.zhu@zaptechnology.com>
2018-03-12 14:47:06 +1000

    Add project files.

---
+++ OctoParamsCounter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OctoParamsCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
